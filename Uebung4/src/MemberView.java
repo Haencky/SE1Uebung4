7219e6a53bab7e846a164bb39a45ae214e2adcfa
Haencky <115699809+Haencky@users.noreply.github.com>
2022-11-05 18:32:46 +0100

    Delete Uebung4 directory

--- Uebung4/src/MemberView.java
+++
@@ -1,9 +0,0 @@
-import java.util.List;
-
-public class MemberView {
-    public void dump (List<Member> liste) {
-        for(Member m : liste) {
-            System.out.println(m.toString());
-        }
-    }
-}
